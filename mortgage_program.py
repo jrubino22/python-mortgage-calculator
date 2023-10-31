fae243834cfb505079e55280bd3caf5d90336517
Joseph Rubino <joeyrubi22@gmail.com>
2023-10-31 17:30:57 -0500

    mortgage calc complete

--- mortgage_program.py
+++ mortgage_program.py
@@ -1,3 +1,17 @@
-print("Let's calculate your mortgage payments!")
-print("What is the loan principal")
-p = input()
+def mortgage_calculation(principal, interest_rate, num_years):
+    p = float(principal)
+    r = (float(interest_rate) / 100) / 12
+    n = float(num_years) * 12
+    rate = p * r * (((1 + r)**n)/(((1+r)**n)-1))
+    return round(rate, 2)
+
+print("Let's calculate your fixed monthly mortgage payments!")
+print("What is the loan amount?")
+principal = input()
+print("What is the annual interest rate?")
+interest_rate = input()
+print("In years, what is the life of this loan?")
+num_years = input()
+mort_rate = mortgage_calculation(principal, interest_rate, num_years)
+final_string = "Your mortgage payment will be ${} per month".format(mort_rate)
+print(final_string)
